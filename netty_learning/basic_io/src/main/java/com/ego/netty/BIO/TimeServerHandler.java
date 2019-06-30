d83a1bd94ae811850310bfff137a26ee221f93d4
adair_chan <adairchan.dream@gmail.com>
2019-06-30 10:04:24 +0800

    Examples of BIO, create TimeServerHandler(BIO示例之创建TimeServerHandler)

---
+++ netty_learning/basic_io/src/main/java/com/ego/netty/BIO/TimeServerHandler.java
@@ -0,0 +1,21 @@
+package com.ego.netty.BIO;
+
+import java.net.Socket;
+
+/**
+ * @author Ego
+ * @version 1.0
+ * @date 2019-06-30 09:55
+ * @Description
+ */
+public class TimeServerHandler implements Runnable {
+
+    public TimeServerHandler(Socket socket) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
