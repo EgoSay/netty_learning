e11ec02608851def333b8e22a222495c9596aff7
adair_chan <adairchan.dream@gmail.com>
2019-08-07 15:10:25 +0800

    add test method

---
+++ netty_learning/basic_io/src/test/com/ego/netty/test/TestIO.java
@@ -0,0 +1,37 @@
+package com.ego.netty.test;
+
+import com.ego.netty.BIO.TimeServerClient;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+/**
+ * @author Ego
+ * @version 1.0
+ * @date 2019-08-04 18:49
+ * @Description
+ */
+public class TestIO {
+    private static AtomicInteger count = new AtomicInteger(0);
+    @Test
+    public void test() {
+        while (true){
+
+            try {
+                long startTime = System.currentTimeMillis();
+                TimeServerClient timeServerClient = new TimeServerClient();
+                timeServerClient.RunTimeServerClient();
+                count.incrementAndGet();
+                long endTime = System.currentTimeMillis();
+                System.out.println("client:" + count + " is Running" + ", it cost " + (endTime - startTime) + "ms");
+
+            } catch (Exception e) {
+                System.out.println("this client need wait.....");
+            }finally {
+
+            }
+
+        }
+    }
+}
